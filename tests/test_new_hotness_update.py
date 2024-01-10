caad9162a3e376c4c9f076c4d73966c63ffe1186
Matej Focko <mfocko@redhat.com>
2024-01-10 22:03:03 +0100

    fix(tests): rename the test to reflect real meaning
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- tests/test_new_hotness_update.py
+++ tests/test_new_hotness_update.py
@@ -8,7 +8,7 @@ from packit_service_fedmsg.consumer import Consumerino
 from tests.spellbook import DATA_DIR
 
 
-def test_pr_comment():
+def test_new_hotness_reaction():
     flexmock(Celery).should_receive("send_task").and_return(flexmock(id="a")).once()
     with open(DATA_DIR / "new_hotness_update.json") as outfile:
         json_msg = json.load(outfile)
