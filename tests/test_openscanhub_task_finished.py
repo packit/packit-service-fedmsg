5d8c890368b693ce46db3248cffe90f54c5f9206
Maja Massarini <mmassari@redhat.com>
2024-10-09 15:01:56 +0200

    Add OpenScanHub task finished message

---
+++ tests/test_openscanhub_task_finished.py
@@ -0,0 +1,20 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+import json
+
+from celery import Celery
+from fedora_messaging import message
+from flexmock import flexmock
+
+from packit_service_fedmsg.consumer import Consumerino
+from tests.spellbook import DATA_DIR
+
+
+def test_task_finished_event():
+    flexmock(Celery).should_receive("send_task").and_return(flexmock(id="a")).once()
+    with open(DATA_DIR / "openscanhub_task_finished.json") as outfile:
+        json_msg = json.load(outfile)
+        msg = message.loads(json.dumps(json_msg))
+        c = Consumerino()
+        c(msg[0])
